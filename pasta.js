35e4aea3ceca848c8eb11b0746333ac6680640a3
Richard Breno <filip_2001_richard2022@hotmail.com>
2022-11-07 12:49:26 -0300

    commit

--- pasta.js
+++ pasta.js
@@ -7,15 +7,14 @@ var img = window.document.querySelector('img#imagem')
 function carregar() {
     if (hora>6 && hora < 12) {
             texto.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
-            fundo.style.background = 'linear-gradient (to left, rgba(200, 187, 12, 0.816), rgba(146, 146, 5, 0.866)'
+            fundo.style.background = 'yellow'
             img.src = 'imagens/manhã.jpeg'
     } else if (hora >= 12 && hora < 18) {
             texto.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
-            fundo.style.background = 'linear-gradient(to left, blue,rgb(12, 12, 98)'
+            fundo.style.background = 'blue'
             img.src = 'imagens/tarde.jpeg'
     } else {
             texto.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
-            fundo.style.background = 'to left, rgb(1, 1, 22),rgb(0, 0, 0)'
-            img.src = 'imagens/noite.jpeg'
+            fundo.style.background = 'blue'
     }
 }
